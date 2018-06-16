3f9ad36303018ddb349a97c13b37a6ce65491ba7
Vlad <vladpochta5@gmail.com>
2018-06-16 15:28:35 +0300

    Add styles loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,6 +9,13 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
       }
     ]
   },
