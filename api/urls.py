f04845622f8f754b53aef819e955abfb8a9037e1
Vlad <vladpochta5@gmail.com>
2018-06-26 22:17:34 +0300

    Remove client, setup basic endpoint for users

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,24 @@
-from django.urls import path
+from django.urls import path, re_path, include
+from django.contrib.auth.models import User
+
+from rest_framework import serializers, routers, viewsets
 
 
-def view(request):
-    pass
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username')
 
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('', view)
+    path('', include(router.urls)),
+    path('auth', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- client/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-const App = () => <p>test</p>;
-
-ReactDOM.render(<App />, document.getElementById('root'));
